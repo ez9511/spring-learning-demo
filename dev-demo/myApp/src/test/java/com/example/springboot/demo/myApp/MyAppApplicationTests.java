5478752f85e8f9adac7e9bb346b63e6859246645
ez9511 <90930957+ez9511@users.noreply.github.com>
2023-04-12 14:19:47 -0400

    Add files via upload
    Initial

---
+++ dev-demo/myApp/src/test/java/com/example/springboot/demo/myApp/MyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot.demo.myApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
